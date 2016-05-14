6e2903d3eb521e591d56692235c1fb7042bc6229
Len Zhong <zhonglianghong@orbbec.com>
2016-05-14 17:10:55 +0800

    update copyright

--- src/astra_exception.cpp
+++ src/astra_exception.cpp
@@ -1,6 +1,7 @@
 /*
  * Software License Agreement (BSD License)
  *
+ *  Copyright (c) 2013, Willow Garage, Inc.
  *  Copyright (c) 2016, Orbbec Ltd.
  *    Tim Liu <liuhua@orbbec.com>
  *
@@ -16,7 +17,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of Orbbec Ltd. nor the names of its
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
