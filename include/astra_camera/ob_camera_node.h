27c86ddbc107b9aba61b46b332fc5eb4efa2c0ef
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2024-05-21 15:05:06 +0000

    remove extra imports

--- include/astra_camera/ob_camera_node.h
+++ include/astra_camera/ob_camera_node.h
@@ -41,9 +41,6 @@
 #include "types.h"
 #include "utils.h"
 #include "uvc_camera_driver.h"
-#include <image_transport/image_transport.h>
-#include <opencv2/highgui/highgui.hpp>
-#include <cv_bridge/cv_bridge.h>
 
 namespace astra_camera {
 
